b83bcb738daabc3118386e54fdf0f31881ac9a23
xiaobiaozhao <zhaoxiaobiao@gmail.com>
2021-03-20 11:02:52 +0800

    init

---
+++ bundle/ngx_lua-0.10.19/src/ngx_http_lua_fork.c
@@ -0,0 +1,10 @@
+#ifndef DDEBUG
+#define DDEBUG 0
+#endif
+#include "ddebug.h"
+
+void ngx_http_lua_fork_api()
+{
+    int i = 42;
+    i++;
+}
---
+++ bundle/ngx_lua-0.10.19/src/ngx_http_lua_fork.h
@@ -0,0 +1,10 @@
+#ifndef _NGX_HTTP_FORK_H_INCLUDED_
+#define _NGX_HTTP_FORK_H_INCLUDED_
+
+#include "ngx_http_lua_common.h"
+
+void ngx_http_lua_fork_api();
+
+#endif /* _NGX_HTTP_FORK_H_INCLUDED_ */
+
+/* vi:set ft=c ts=4 sw=4 et fdm=marker: */
